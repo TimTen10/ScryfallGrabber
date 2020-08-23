e3dfd30bb5f0b4d47cb7544a9f90dfa8431d4be8
TimTen10 <Tim.Eckle94@gmail.com>
2020-08-23 19:19:11 +0200

    Simple Scryfall API get implemented

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def scryfall_search(cardname):
+    resp = requests.get('https://api.scryfall.com/cards/search', params={'q': cardname})
+    if resp.status_code == 200:
+        card = resp.json()
+        print(f'Name: {card["name"]}, Coloridentity: {card["color_identity"]}, Cmc: {card["cmc"]}, '
+              f'Oracletext: {card["oracle_text"]}')
+    else:
+        print(f'Something went wrong when trying to communicate with the API: {resp.status_code}')
+        print(resp.url)
 
 
-# Press the green button in the gutter to run the script.
+def cardsearch():
+    print('What card are you looking for?')
+    cardname = input()
+    scryfall_search(cardname)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cardsearch()
