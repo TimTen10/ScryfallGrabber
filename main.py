4a9b0a727482150ffc0dc65751a54170eca90c93
TimTen10 <Tim.Eckle94@gmail.com>
2020-08-28 17:03:01 +0200

    change to search

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import requests
 
 
-def scryfall_search(cardname):
-    resp = requests.get('https://api.scryfall.com/cards/search', params={'q': cardname})
+def scryfall_search(search_term):
+    resp = requests.get('https://api.scryfall.com/cards/search', params={'q': search_term})
     if resp.status_code == 200:
-        card = resp.json()
-        print(f'Name: {card["name"]}, Coloridentity: {card["color_identity"]}, Cmc: {card["cmc"]}, '
-              f'Oracletext: {card["oracle_text"]}')
+        res = resp.json()
+        return res
     else:
         print(f'Something went wrong when trying to communicate with the API: {resp.status_code}')
         print(resp.url)
 
 
 def cardsearch():
-    print('What card are you looking for?')
-    cardname = input()
-    scryfall_search(cardname)
+    # The search terminology is the same as on Scryfall! See: https://scryfall.com/docs/syntax
+    print('Input your search.')
+    search_term = input()
+    res = scryfall_search(search_term)
+    for card in res['data']:
+        print(card)
 
 
 if __name__ == '__main__':
